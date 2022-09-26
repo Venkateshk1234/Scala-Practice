88b5f96b9cb9c2710eb354dfe0a9611c8ddcaed0
Venkateshk1234 <113977096+Venkateshk1234@users.noreply.github.com>
2022-09-26 17:15:41 +0530

    Add files via upload

---
+++ Strings/ReverseString.scala
@@ -0,0 +1,16 @@
+
+
+object ReverseString {
+  def reverseString(newString: String): String = {
+        var revString = ""
+        val n = newString.length()
+        for(i <- 0 to n-1){
+            revString = revString.concat(newString.charAt(n-i-1).toString)
+        }
+    return revString
+    }
+    def main(args: Array[String]) {
+        var newString = "Affine Analytics"
+        println("Reverse of '" + newString + "' is '" + reverseString(newString) + "'")
+    }
+}
