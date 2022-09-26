afdfb70bbaa7405918cba7dfa650689bc816185e
Venkateshk1234 <113977096+Venkateshk1234@users.noreply.github.com>
2022-09-26 20:48:32 +0530

    Add files via upload

---
+++ Loops/Forloops/UntilKeyWordInForLoop.scala
@@ -0,0 +1,12 @@
+// Scala program to demonstrate 
+// the until keyword in for loop
+
+object UntilKeyWordInForLoop {
+  def main(args: Array[String]) {  
+        for( cnt <- 1 until 10 )
+        {
+            printf("%d ",cnt);
+        }
+        println()
+    }
+}
