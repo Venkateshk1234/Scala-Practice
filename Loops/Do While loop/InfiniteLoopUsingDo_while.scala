2d4026e069f678c8e7ce7fef9b191309fd096eea
Venkateshk1234 <113977096+Venkateshk1234@users.noreply.github.com>
2022-09-26 19:33:58 +0530

    Add files via upload

---
+++ Loops/Do While loop/InfiniteLoopUsingDo_while.scala
@@ -0,0 +1,11 @@
+// Scala program to implement infinite loop 
+// using "do-while" loop
+
+object InfiniteLoopUsingDo_while {
+  def main(args: Array[String]) {  
+        do
+        {
+            printf("Hello")
+        }while(true)
+    }
+}
